7fd38403a8d5a456f49d38fc1f2edc086d9c6c9d
Szabolcs Peter <szabi.peter@gmail.com>
2025-07-20 10:57:40 +0200

    Configure Producer Ack

--- ProductsMicroservice/src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
+++ ProductsMicroservice/src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
@@ -37,7 +37,7 @@ public class ProductServiceImpl implements ProductService {
         LOGGER.info("Before publishing a ProductCreatedEvent");
 
         SendResult<String, ProductCreatedEvent> result =
-                kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent).get();
+                kafkaTemplate.send("topic2", productId, productCreatedEvent).get();
 
         LOGGER.info("Partition: " + result.getRecordMetadata().partition());
         LOGGER.info("Topic: " + result.getRecordMetadata().topic());
