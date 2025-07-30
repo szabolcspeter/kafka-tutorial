064ca31f372537d6c54164ceb0a0c220ee85e04a
Szabolcs Peter <szabi.peter@gmail.com>
2025-07-30 11:47:14 +0200

    Register Retryable exception and define wait time interval

--- EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/KafkaConsumerConfiguration.java
+++ EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/KafkaConsumerConfiguration.java
@@ -1,6 +1,7 @@
 package com.appsdeveloperblog.ws.EmailNotificationMicroservice;
 
 import com.appsdeveloperblog.ws.EmailNotificationMicroservice.error.NotRetryableException;
+import com.appsdeveloperblog.ws.EmailNotificationMicroservice.error.RetryableException;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -16,6 +17,7 @@ import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -48,8 +50,10 @@ public class KafkaConsumerConfiguration {
             ConsumerFactory<String, Object> consumerFactory,
             KafkaTemplate<String, Object> kafkaTemplate) {
 
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate));
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate),
+                new FixedBackOff(5000, 3));
         errorHandler.addNotRetryableExceptions(NotRetryableException.class);
+        errorHandler.addRetryableExceptions(RetryableException.class);
 
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
