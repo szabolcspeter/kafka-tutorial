2b0f362b0f955df23a8560abf2747f24e14aacbd
Szabolcs Peter <szabi.peter@gmail.com>
2025-07-30 12:16:40 +0200

    Throw retryable exception

--- EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/EmailNotificationMicroserviceApplication.java
+++ EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/EmailNotificationMicroserviceApplication.java
@@ -2,6 +2,8 @@ package com.appsdeveloperblog.ws.EmailNotificationMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmailNotificationMicroserviceApplication {
@@ -10,4 +12,8 @@ public class EmailNotificationMicroserviceApplication {
 		SpringApplication.run(EmailNotificationMicroserviceApplication.class, args);
 	}
 
+	@Bean
+	RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
