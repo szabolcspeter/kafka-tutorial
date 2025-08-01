b23ab62f00c3ce9f3bd8a3350258c92af49f888b
Szabolcs Peter <szabi.peter@gmail.com>
2025-08-01 10:02:55 +0200

    Testing consumer group with 3 instances of EmailNotification microservice

--- EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/KafkaConsumerConfiguration.java
+++ EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/KafkaConsumerConfiguration.java
@@ -40,7 +40,7 @@ public class KafkaConsumerConfiguration {
         config.put(JsonDeserializer.TRUSTED_PACKAGES,
                 environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
         config.put(ConsumerConfig.GROUP_ID_CONFIG,
-                environment.getProperty("spring.kafka.consumer.group-id"));
+                environment.getProperty("consumer.group-id"));
 
         return new DefaultKafkaConsumerFactory<>(config);
     }
