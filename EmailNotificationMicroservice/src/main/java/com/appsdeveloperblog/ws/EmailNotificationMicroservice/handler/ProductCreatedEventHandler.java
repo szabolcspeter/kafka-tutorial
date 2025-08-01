97d576777aabaf720b70a280c4977334918bd166
Szabolcs Peter <szabi.peter@gmail.com>
2025-08-01 11:21:47 +0200

    Log productId in EmailNotification Kafka consumer microservice

--- EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/handler/ProductCreatedEventHandler.java
+++ EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/handler/ProductCreatedEventHandler.java
@@ -28,7 +28,8 @@ public class ProductCreatedEventHandler {
 
     @KafkaHandler
     public void handle(ProductCreatedEvent productCreatedEvent) {
-        LOGGER.info("Received a new event: " + productCreatedEvent.getTitle());
+        LOGGER.info("Received a new event: " + productCreatedEvent.getTitle() +
+                " with productId: " + productCreatedEvent.getProductId());
 
         String requestUrl = "http://localhost:8082/response/200";
         try {
