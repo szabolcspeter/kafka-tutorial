10f1db4e9c03eea11026e5a6b849c37a1b189493
Szabolcs Peter <szabi.peter@gmail.com>
2025-08-06 16:55:59 +0200

    JPA Repository created

---
+++ EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/io/ProcessedEventRepository.java
@@ -0,0 +1,8 @@
+package com.appsdeveloperblog.ws.EmailNotificationMicroservice.io;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEventEntity, Long> {
+}
